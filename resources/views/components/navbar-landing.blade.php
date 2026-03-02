f93df3d5c5d730d73b38d545e3ce992cef173274
zayennn <zaayeennn@gmail.com>
2026-03-03 01:37:22 +0700

    update

--- resources/views/components/navbar-landing.blade.php
+++ resources/views/components/navbar-landing.blade.php
@@ -4,9 +4,6 @@
     <!-- Nav Menu Desktop -->
     <nav class="nav__menu">
         <a href="#home" class="nav__link" data-route="/">Home</a>
-        <a href="#bahasa" class="nav__link" data-route="/bahasa">Bahasa Pemrogramman</a>
-        <a href="#tentang" class="nav__link" data-route="/tentang">Tentang</a>
-        <a href="#roadmap" class="nav__link" data-route="/roadmap">Roadmap</a>
 
         <div class="nav-actions-desktop">
             <div class="theme-toggle">
